da0f6694c23cba842d47064d85461605649bd0e9
Bruno <bruno.eng.comp@gmail.com>
2014-11-02 17:37:48 -0200

    Criacao do controller scaffold estatico da classe conta

---
+++ grails-app/controllers/br/ufscar/dc/dsw/ContaController.groovy
@@ -0,0 +1,104 @@
+package br.ufscar.dc.dsw
+
+
+
+import static org.springframework.http.HttpStatus.*
+import grails.transaction.Transactional
+
+@Transactional(readOnly = true)
+class ContaController {
+
+    static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
+
+    def index(Integer max) {
+        params.max = Math.min(max ?: 10, 100)
+        respond Conta.list(params), model:[contaInstanceCount: Conta.count()]
+    }
+
+    def show(Conta contaInstance) {
+        respond contaInstance
+    }
+
+    def create() {
+        respond new Conta(params)
+    }
+
+    @Transactional
+    def save(Conta contaInstance) {
+        if (contaInstance == null) {
+            notFound()
+            return
+        }
+
+        if (contaInstance.hasErrors()) {
+            respond contaInstance.errors, view:'create'
+            return
+        }
+
+        contaInstance.save flush:true
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.created.message', args: [message(code: 'conta.label', default: 'Conta'), contaInstance.id])
+                redirect contaInstance
+            }
+            '*' { respond contaInstance, [status: CREATED] }
+        }
+    }
+
+    def edit(Conta contaInstance) {
+        respond contaInstance
+    }
+
+    @Transactional
+    def update(Conta contaInstance) {
+        if (contaInstance == null) {
+            notFound()
+            return
+        }
+
+        if (contaInstance.hasErrors()) {
+            respond contaInstance.errors, view:'edit'
+            return
+        }
+
+        contaInstance.save flush:true
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.updated.message', args: [message(code: 'Conta.label', default: 'Conta'), contaInstance.id])
+                redirect contaInstance
+            }
+            '*'{ respond contaInstance, [status: OK] }
+        }
+    }
+
+    @Transactional
+    def delete(Conta contaInstance) {
+
+        if (contaInstance == null) {
+            notFound()
+            return
+        }
+
+        contaInstance.delete flush:true
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.deleted.message', args: [message(code: 'Conta.label', default: 'Conta'), contaInstance.id])
+                redirect action:"index", method:"GET"
+            }
+            '*'{ render status: NO_CONTENT }
+        }
+    }
+
+    protected void notFound() {
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.not.found.message', args: [message(code: 'conta.label', default: 'Conta'), params.id])
+                redirect action: "index", method: "GET"
+            }
+            '*'{ render status: NOT_FOUND }
+        }
+    }
+}
