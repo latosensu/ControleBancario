c52d7d6e53eb05187dc28c0e69ac3c080b7d64c4
Bruno <bruno.eng.comp@gmail.com>
2014-11-03 13:08:19 -0200

    Criacao da classe ClienteFisico, sem validacao do cpf

---
+++ grails-app/domain/br/ufscar/dc/dsw/ClienteFisico.groovy
@@ -0,0 +1,16 @@
+package br.ufscar.dc.dsw
+
+class ClienteFisico extends Cliente{
+
+    static constraints = {
+        cpf (blank: false, unique: true, /*cpf:true, */size: 14..14)
+        rg (blank: false, size: 1..12)
+    }
+
+    String rg
+    String cpf
+
+    String toString() {
+        return cpf
+    }
+}
