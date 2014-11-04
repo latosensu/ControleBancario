19f3399350b607f0919fb0ff4e3edaa3797f645f
Bruno <bruno.eng.comp@gmail.com>
2014-11-04 12:08:36 -0200

    Metodo para emitir extrato de conta para um numero definido de dias

--- grails-app/services/br/ufscar/dc/dsw/ExtratoService.groovy
+++ grails-app/services/br/ufscar/dc/dsw/ExtratoService.groovy
@@ -9,7 +9,28 @@ class ExtratoService {
     public static final Integer EXTRATO_ULTIMOS_15_DIAS = 15
     public static final Integer EXTRATO_ULTIMOS_30_DIAS = 30
 
-    def serviceMethod() {
+    def emitirExtrato(ContaCliente contaCliente, Integer tipoExtrato) {
+        int numeroDias = EXTRATO_ULTIMOS_7_DIAS
+        switch (tipoExtrato) {
+            case EXTRATO_ULTIMOS_7_DIAS:
+                break
+            case EXTRATO_ULTIMOS_15_DIAS:
+                numeroDias = EXTRATO_ULTIMOS_15_DIAS
+                break
+            case EXTRATO_ULTIMOS_30_DIAS:
+                numeroDias = EXTRATO_ULTIMOS_30_DIAS
+                break
+        }
+
+        Calendar calendar = Calendar.getInstance(); //data atual
+        Date dataMaxima = calendar.time
+        calendar.add(-numeroDias, Calendar.DAY_OF_YEAR); //subtraindo o número de dias selecionado
+        Date dataMinima = calendar.time
+
+        return contaCliente.transacoes.findResults {
+            le('data', dataMaxima)
+            ge('data', dataMinima)
+        }
 
     }
 }
