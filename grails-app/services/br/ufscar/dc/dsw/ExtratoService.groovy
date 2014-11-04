fb345ae6f2beb4db3a74b445cfe5338d8ee1e7a3
Bruno <bruno.eng.comp@gmail.com>
2014-11-04 11:48:20 -0200

    Definicao das constantes para opcoes de extrato

--- grails-app/services/br/ufscar/dc/dsw/ExtratoService.groovy
+++ grails-app/services/br/ufscar/dc/dsw/ExtratoService.groovy
@@ -5,6 +5,10 @@ import grails.transaction.Transactional
 @Transactional
 class ExtratoService {
 
+    public static final Integer EXTRATO_ULTIMOS_7_DIAS = 7
+    public static final Integer EXTRATO_ULTIMOS_15_DIAS = 15
+    public static final Integer EXTRATO_ULTIMOS_30_DIAS = 30
+
     def serviceMethod() {
 
     }
