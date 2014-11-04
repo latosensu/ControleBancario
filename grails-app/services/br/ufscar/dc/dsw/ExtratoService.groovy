5c4d1f86efe2830b12a29b5620a9691f24dfecf6
Bruno <bruno.eng.comp@gmail.com>
2014-11-04 11:45:48 -0200

    Adicao do ExtratoService

---
+++ grails-app/services/br/ufscar/dc/dsw/ExtratoService.groovy
@@ -0,0 +1,11 @@
+package br.ufscar.dc.dsw
+
+import grails.transaction.Transactional
+
+@Transactional
+class ExtratoService {
+
+    def serviceMethod() {
+
+    }
+}
---
+++ test/unit/br/ufscar/dc/dsw/ExtratoServiceSpec.groovy
@@ -0,0 +1,20 @@
+package br.ufscar.dc.dsw
+
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+
+/**
+ * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
+ */
+@TestFor(ExtratoService)
+class ExtratoServiceSpec extends Specification {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+    }
+}
