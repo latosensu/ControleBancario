e3fc444bfd0b91303ec3f325e82d2945dbd41160
Bruno <bruno.eng.comp@gmail.com>
2014-11-03 23:26:31 -0200

    Adicao de plugin para validacao de cpf

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -72,5 +72,8 @@ grails.project.dependency.resolution = {
         //compile ":less-asset-pipeline:1.10.0"
         //compile ":coffee-asset-pipeline:1.8.0"
         //compile ":handlebars-asset-pipeline:1.3.0.3"
+
+        //plugin de validação
+        compile ":br-validation:0.3"
     }
 }
--- grails-app/domain/br/ufscar/dc/dsw/ClienteFisico.groovy
+++ grails-app/domain/br/ufscar/dc/dsw/ClienteFisico.groovy
@@ -3,7 +3,7 @@ package br.ufscar.dc.dsw
 class ClienteFisico extends Cliente{
 
     static constraints = {
-        cpf (blank: false, unique: true, /*cpf:true, */size: 14..14)
+        cpf (blank: false, unique: true, cpf:true, size: 14..14)
         rg (blank: false, size: 1..12)
     }
 
